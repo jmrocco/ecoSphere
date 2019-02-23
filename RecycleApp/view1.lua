c13971d28ceb888a4e067bb86bfed3e956552fb0
CalebBrett <43529463+CalebBrett@users.noreply.github.com>
2019-02-23 15:32:36 -0500

    changes screens

--- RecycleApp/view1.lua
+++ RecycleApp/view1.lua
@@ -11,52 +11,89 @@ function scene:create( event )
 	local sceneGroup = self.view
 	local background
 	local fish
+	local fish2
+
 	if points >= 20 then
 		background = display.newImageRect( "perfecteco.jpg", 415, 646 )
 		background.x = display.contentCenterX - 20
 		background.y = display.contentCenterY- 25
 		fish = display.newImageRect( "fish.png", 60, 30 )
+		fish2 = display.newImageRect( "fish.png", 60, 30 )
 
 	elseif points >= 10 then
 		background = display.newImageRect( "middleeco.jpg", 415, 646 )
 		background.x = display.contentCenterX - 20
 		background.y = display.contentCenterY- 25
 		fish = display.newImageRect( "fish.png", 60, 30 )
+		fish2 = display.newImageRect( "dead fish.png", 60, 30 )
 
 	else
 		background = display.newImageRect( "worsteco.jpg", 415, 646 )
 		background.x = display.contentCenterX - 20
 		background.y = display.contentCenterY - 25
 		fish = display.newImageRect( "dead fish.png", 60, 30 )
+		fish2 = display.newImageRect( "dead fish.png", 60, 30 )
 
  	end
+ 	fish2.x = 100
  	fish.y = 400
- 	local count = 20000
+ 	fish2.y = 450
+ 	local count = 99999999999999999999999999999999
  	local back = false
+ 	local back2 = false
+
 	local function swim()
-		count = count + 100
 		if fish.x < 0 then
 	    	back = false
 		end
 		if fish.x > display.actualContentWidth then
 	    	back = true
+	    	fish.xScale = -1
 		end
 		if back == false then
 	    	fish.x = fish.x + 1
+	    	fish.xScale = 1
 		end
 		if back == true then
 	    	fish.x = fish.x - 1
 		end
 		fish.y = (math.sin(fish.x/30) * 20) + 180
-		if points > 30 then
+		if points < 30 then
 			fish.y = fish.y + (points * 10)
+		else
+			fish.y = fish.y + 300
+		end
+
+		if fish2.x < 0 then
+	    	back2 = false
+		end
+		if fish2.x > display.actualContentWidth then
+	    	back2 = true
+	    	fish2.xScale = -1
+		end
+		if back2 == false then
+	    	fish2.x = fish2.x + 1
+	    	fish2.xScale = 1
+		end
+		if back2 == true then
+	    	fish2.x = fish2.x - 1
+		end
+		fish2.y = (math.sin(fish2.x/30) * 20) + 180
+		if points < 30 then
+			fish2.y = fish2.y + (points * 5)
+		else
+			fish2.y = fish2.y + 300
 		end
 	end
 
 	timer.performWithDelay(10, swim, count)
+	local scoreText = display.newText(points, display.contentCenterX, 20, native.systemFont, 60 )
+	scoreText:setFillColor( 0, 0, 0 )
 
 	sceneGroup:insert( background )
 	sceneGroup:insert( fish )
+	sceneGroup:insert( fish2 )
+
 
 end
 
